b6e14b0433bc9ec8c652f293f5347ec99c492c12
jailakshmi7683 <kangulajailakshmi0314@gmail.com>
2025-12-30 15:55:32 +0530

    Header and Footer is ready

--- src/app/core/layout/header/header.ts
+++ src/app/core/layout/header/header.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrl: './header.css'
 })
 export class Header {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
 }
